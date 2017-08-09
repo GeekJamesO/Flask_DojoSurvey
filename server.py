08ea713aad2ce4612350998729cdedb8b21187c9
JamesORourke <geekjamesorourkejr@icloud.com>
2017-08-08 18:27:17 -0700

    Added Validation

--- server.py
+++ server.py
@@ -1,24 +1,40 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, flash
 app = Flask(__name__)
+app.secret_key="SmokeandFire"
 
-@app.route("/", methods=['GET'])
+@app.route('/', methods=['GET'])
 def root():
     return render_template("DojoSurvey.html")
 
 @app.route("/result", methods=['POST'])
 def result():
-    print 'before Params'
     resultName = request.form['Name']
-    print "name = ", resultName
     resultLocation = request.form['Dojos']
-    print "location = ", resultLocation
     resultLanguage = request.form['Language']
-    print "language = ", resultLanguage
     resultComment = request.form['Comment']
-    print "comment = ", resultComment
-    return render_template("result.html",
-                           Name=resultName,
-                           Location=resultLocation,
-                           Language=resultLanguage,
-                           Comment=resultComment)
+
+    # validate
+    validInput = True
+    if (len(resultName) < 1):
+        validInput = False;
+        flash("Name field cannot be blank.")
+
+
+
+    if (len(resultComment) < 1):
+        validInput = False;
+        flash("Comment field cannot be blank.")
+    if (len(resultComment) > 120):
+        validInput = False;
+        flash("Comment field cannot be larger than 120 characters.")
+
+
+    if (validInput):
+        return render_template('result.html',
+                               Name=resultName,
+                               Location=resultLocation,
+                               Language=resultLanguage,
+                               Comment=resultComment)
+    else:
+        return redirect('/')
 app.run(debug=True)
